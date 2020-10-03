7b7e86d09be7c42740181cf340d5b9fe50b4ba20
wjdtj9656 <wjdtj9656@naver.com>
2020-10-03 20:41:56 +0900

    10/3 update

--- app/src/main/java/com/example/bottomnavi/Frag3.java
+++ app/src/main/java/com/example/bottomnavi/Frag3.java
@@ -1,5 +1,6 @@
 package com.example.bottomnavi;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,9 +10,20 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.github.mikephil.charting.animation.Easing;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+
 public class Frag3 extends Fragment {
 
     private View view;
+    PieChart pieChart;
 
     @Nullable
     @Override
@@ -23,6 +35,47 @@ public class Frag3 extends Fragment {
         String userID = bundle.getString("userID");
         String userEmail = bundle.getString("userEmail");
         String userPass = bundle.getString("userPass");
+
+        /*
+        pieChart = (PieChart) getView().findViewById(R.id.piechart);
+
+        pieChart.setUsePercentValues(true);
+        pieChart.getDescription().setEnabled(false);
+        pieChart.setExtraOffsets(5,10,5,5);
+
+        pieChart.setDragDecelerationFrictionCoef(0.95f);
+
+        pieChart.setDrawHoleEnabled(false);
+        pieChart.setHoleColor(Color.WHITE);
+        pieChart.setTransparentCircleRadius(61f);
+
+        ArrayList<PieEntry> yValues = new ArrayList<PieEntry>();
+
+        yValues.add(new PieEntry(34f,"Japen"));
+        yValues.add(new PieEntry(23f,"USA"));
+        yValues.add(new PieEntry(14f,"UK"));
+        yValues.add(new PieEntry(35f,"India"));
+        yValues.add(new PieEntry(40f,"Russia"));
+        yValues.add(new PieEntry(40f,"Korea"));
+
+        Description description = new Description();
+        description.setText("세계 국가"); //라벨
+        description.setTextSize(15);
+        pieChart.setDescription(description);
+
+        pieChart.animateY(1000, Easing.EasingOption.EaseInOutCubic); //애니메이션
+
+        PieDataSet dataSet = new PieDataSet(yValues,"Countries");
+        dataSet.setSliceSpace(3f);
+        dataSet.setSelectionShift(5f);
+        dataSet.setColors(ColorTemplate.JOYFUL_COLORS);
+
+        PieData data = new PieData((dataSet));
+        data.setValueTextSize(10f);
+        data.setValueTextColor(Color.YELLOW);
+
+        pieChart.setData(data);
+*/
         return view;
     }
 }
